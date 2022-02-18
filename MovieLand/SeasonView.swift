6150f006f498181288cb3518e62f92f80a3ea4c0
DyDyDev <dylan68@sfr.fr>
2022-02-18 15:39:57 +0100

    SeasonCellFinish

--- MovieLand/SeasonView.swift
+++ MovieLand/SeasonView.swift
@@ -16,13 +16,31 @@ struct SeasonView: View {
     let description: String
     
     var body: some View {
-        HStack {
+        HStack(spacing: 15) {
             KFImage(imageURL)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 100, height: 150)
                 .cornerRadius(8.0)
+            VStack(alignment: .leading, spacing: 11) {
+                VStack(alignment: .leading, spacing: 1) {
+                    Text("Saison \(seasonNumber)")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Text("\(episodesCount) épisodes")
+                        .foregroundColor(Color.white.opacity(0.5))
+                        .font(.caption)
+                        .lineLimit(1)
+                } // VSTACK
+                Text(description)
+                    .font(.callout)
+                    .fontWeight(.light)
+                    .lineLimit(4)
+            } // VSTACK
+            Spacer()
         } // HSTACK
+        .foregroundColor(.white)
+        .padding(8)
     }
 }
 
@@ -30,11 +48,11 @@ struct SeasonView_Previews: PreviewProvider {
     static var previews: some View {
         SeasonView(
             imageURL: URL(string: "https://www.themoviedb.org/t/p/w260_and_h390_bestv2/lXS60geme1LlEob5Wgvj3KilClA.jpg"),
-            seasonNumber: 3,
+            seasonNumber: 2,
             episodesCount: 9,
             description: "La saison 2 de Stranger Things a été diffusée à partir du 27 octobre 2017."
         )
-            .previewLayout(.sizeThatFits)
-            .padding()
+        .previewLayout(.sizeThatFits)
+        .background(Color(red: 88/255, green: 32/255, blue: 53/255))
     }
 }
