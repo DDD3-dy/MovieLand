bba16ea1e0caa2187463542348860cdcf46f15dc
DyDyDev <dylan68@sfr.fr>
2022-02-19 11:21:14 +0100

    MovieCellModel

---
+++ MovieLand/MovieCellModel.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCellModel.swift
+//  MovieLand
+//
+//  Created by Dylan Caetano on 19/02/2022.
+//
+
+import Foundation
+
+struct MovieCellModel {
+    let imageURL: URL?
+    let title: String
+    let seasonCount: Int
+    let genres: [String]
+    let rating: Float
+    let description: String
+}
--- MovieLand/MovieCellView.swift
+++ MovieLand/MovieCellView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 import Kingfisher
 
 struct MovieCellView: View {
+    
+    let viewModel: MovieCellModel
+    
     var body: some View {
         HStack {
-            KFImage(URL(string: "https://www.themoviedb.org/t/p/w1280/joCCbrgiXbVxcpnWOzrIwNz8tC6.jpg"))
+            KFImage(viewModel.imageURL)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 100, height: 150)
@@ -20,20 +23,20 @@ struct MovieCellView: View {
             VStack(alignment: .leading, spacing: 8) {
                 HStack {
                     VStack(alignment: .leading) {
-                        Text("Stranger Things")
+                        Text(viewModel.title)
                             .font(.title2)
                             .fontWeight(.semibold)
-                        Text("Saison + types + ect")
+                        Text("\(viewModel.seasonCount) saison | \(viewModel.genres.joined(separator: " • "))")
                             .foregroundColor(Color.white.opacity(0.5))
                             .font(.caption)
                             .lineLimit(1)
                     } // VSTACK
                     Spacer()
-                    CircleProgressBarView(progress: 8.3)
+                    CircleProgressBarView(progress: viewModel.rating)
                         .frame(width: 45, height: 45)
                         .padding(2.5)
                 } // HSTACK
-                Text("Quand un jeune garçon disparaît, une petite ville découvre une affaire mystérieuse, des expériences secrètes, des forces surnaturelles terrifiantes... et une fillette.")
+                Text(viewModel.description)
                     .font(.callout)
                     .fontWeight(.light)
                     .lineLimit(4)
@@ -46,8 +49,17 @@ struct MovieCellView: View {
 }
 
 struct MovieCellView_Previews: PreviewProvider {
+    
+    static let viewModel = MovieCellModel(
+        imageURL: URL(string: "https://www.themoviedb.org/t/p/w1280/joCCbrgiXbVxcpnWOzrIwNz8tC6.jpg"),
+        title: "Stranger Things",
+        seasonCount: 3,
+        genres: ["Drame", "Mystère", "Science-Fiction", "Fantastique"],
+        rating: 8.6,
+        description: "La saison 2 de Stranger Things a été diffusée à partir du 27 octobre 2017.")
+    
     static var previews: some View {
-        MovieCellView()
+        MovieCellView(viewModel: viewModel)
             .previewLayout(.sizeThatFits)
     }
 }
